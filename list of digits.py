33d50f6c268e7f55d84e8989ec68b9b977f4c28e
ZainebPenwala <52603780+ZainebPenwala@users.noreply.github.com>
2019-07-16 18:53:25 +0530

    Create list of digits.py

---
+++ list of digits.py
@@ -0,0 +1,13 @@
+# a function that takes a number and prints a list of its digits. Eg. if number is 250, then it should print:
+2
+5
+0
+
+
+a=(input('enter no '))
+def li(s):
+    for elem in a:
+        print(elem)
+        
+li=li(a)
+
