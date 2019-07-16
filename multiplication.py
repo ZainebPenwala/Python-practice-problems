7e90089da2341bfb11a024ebb84a6aff965985f0
ZainebPenwala <52603780+ZainebPenwala@users.noreply.github.com>
2019-07-16 18:33:59 +0530

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -5,3 +5,12 @@ for a in range(1,101):
         print(a,'x',b,'=',a*b)
     print('-'*10)
 
+
+    
+# a program that prints a multiplication table for numbers up to 12.
+
+for a in range(1,13):
+    for b in range(1,11):
+        print(a,'x',b,'=',a*b)
+    print('-'*10)
+    
