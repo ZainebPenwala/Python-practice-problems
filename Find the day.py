125c7072884688daae6962f9f6a248cbf94fdc82
Zaineb <zenp1520@gmail.com>
2019-09-20 12:49:36 +0530

    Update Find the day.py

--- Find the day.py
+++ Find the day.py
@@ -1,6 +1,11 @@
 # From the given date find the day of on that date.
 # { %A = day , %d = date, %b = month, %y = year}
 
+
+import pandas as pd 
+import numpy as np
+from datetime import datetime
+
 date = "20Sept1959"
 a=pd.to_datetime(date)
 print(a)
